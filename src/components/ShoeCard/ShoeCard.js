d6ebdf602812857928abe75bb877d88c576ad0c2
Michael Kostal <kostal@gmail.com>
2024-01-14 17:03:14 -0600

    ShoeGrid

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,15 +53,21 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex:1 1 25%;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  display:block;
+  max-width:100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
